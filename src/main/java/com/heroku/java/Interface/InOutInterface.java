3d6a56b43860767284c479fed660318c1451b52a
송찬영 <52197226+payo03@users.noreply.github.com>
2024-12-31 10:41:13 +0900

    Heroku Quotaguard Test

--- src/main/java/com/heroku/java/Interface/InOutInterface.java
+++ src/main/java/com/heroku/java/Interface/InOutInterface.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,7 +45,36 @@ public class InOutInterface {
         headers.set("Content-Type", "application/json");
         headers.set("Authorization", "App ca9697740134af524df3d4a4cf702337-e938db19-144d-4a33-90d0-fb349dae8c2d");
 
-        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(infoMap, headers);
+        Map<String, Object> body = new HashMap<>();
+        body.put("messages", Arrays.asList(
+            Map.of(
+                "sender", "85d506c54e71a52daa4598efb194ac5ed21732d8",
+                "destinations", Arrays.asList(
+                    Map.of("to", "821012345666")
+                ),
+                "options", Map.of(
+                    "validityPeriod", Map.of("amount", "15"),
+                    "smsFailover", Map.of(
+                        "sender", "031-8014-5700",
+                        "text", "Sample Template Test\nUserName : payo03@man.eu.trial.partial"
+                    )
+                ),
+                "webhooks", Map.of(
+                    "contentType", "application/json",
+                    "delivery", Map.of(
+                        "url", "https://app-force-1035--partial.sandbox.my.salesforce-sites.com/guest/services/apexrest/api/webhook/kakao",
+                        "intermediateReport", "true"
+                    )
+                ),
+                "content", Map.of(
+                    "text", "Sample Template Test\nUserName : payo03@man.eu.trial.partial",
+                    "templateCode", "KAKAO000",
+                    "type", "TEMPLATE"
+                )
+            )
+        ));
+        
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(body, headers);
         try {
             ResponseEntity<String> response = restTemplate.exchange(
                 url, 
@@ -59,6 +89,7 @@ public class InOutInterface {
             resultMap.put("code", false);
             resultMap.put("message", e.getMessage());
         }
+        headers.forEach((key, value) -> logger.info(key + ": " + value));
         logger.info(resultMap);
 
         return resultMap;
