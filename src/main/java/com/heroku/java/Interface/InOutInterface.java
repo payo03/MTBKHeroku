6d8ef134ee5f593f91028c3d9a1cd77be1cbf499
송찬영 <52197226+payo03@users.noreply.github.com>
2024-12-31 10:50:26 +0900

    Heroku Quotaguard Test

--- src/main/java/com/heroku/java/Interface/InOutInterface.java
+++ src/main/java/com/heroku/java/Interface/InOutInterface.java
@@ -28,15 +28,11 @@ public class InOutInterface {
     private RestTemplate restTemplate;
 
     @PostMapping("/kakao/alim")
-    public Map<String, Object> kakaoAlim(@RequestParam Map<String, Object> infoMap) {
+    public Map<String, Object> kakaoAlim(@RequestParam String jsonString) {
         Map<String, Object> resultMap = new HashMap<String, Object>();
         resultMap.put("code", true);
         resultMap.put("message", "Great. you\'ve got " + ((int) (Math.random() * 100)) + " points");
-        resultMap.put("infoMap : ", infoMap);
-
-        /*
-            Kakaotalk API 호출
-        */
+        resultMap.put("infoMap : ", jsonString);
 
         String url = "https://pe86m3.api-id.infobip.com/kakao-alim/1/messages";
 
@@ -44,37 +40,8 @@ public class InOutInterface {
         headers.set("Accept", "application/json");
         headers.set("Content-Type", "application/json");
         headers.set("Authorization", "App ca9697740134af524df3d4a4cf702337-e938db19-144d-4a33-90d0-fb349dae8c2d");
-
-        Map<String, Object> body = new HashMap<>();
-        body.put("messages", Arrays.asList(
-            Map.of(
-                "sender", "85d506c54e71a52daa4598efb194ac5ed21732d8",
-                "destinations", Arrays.asList(
-                    Map.of("to", "821012345666")
-                ),
-                "options", Map.of(
-                    "validityPeriod", Map.of("amount", "15"),
-                    "smsFailover", Map.of(
-                        "sender", "031-8014-5700",
-                        "text", "Sample Template Test\nUserName : payo03@man.eu.trial.partial"
-                    )
-                ),
-                "webhooks", Map.of(
-                    "contentType", "application/json",
-                    "delivery", Map.of(
-                        "url", "https://app-force-1035--partial.sandbox.my.salesforce-sites.com/guest/services/apexrest/api/webhook/kakao",
-                        "intermediateReport", "true"
-                    )
-                ),
-                "content", Map.of(
-                    "text", "Sample Template Test\nUserName : payo03@man.eu.trial.partial",
-                    "templateCode", "KAKAO000",
-                    "type", "TEMPLATE"
-                )
-            )
-        ));
         
-        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        HttpEntity<String> requestEntity = new HttpEntity<>(jsonString, headers);
         try {
             ResponseEntity<String> response = restTemplate.exchange(
                 url, 
